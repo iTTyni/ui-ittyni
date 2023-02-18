4754e09a3bc580f8b85e4f93b9429d91d65ee6a9
mkhmalech <m.khmalech@gmail.com>
2023-02-18 14:52:41 +0100

    add dektop designs

--- stories/screen/product_details.stories.tsx
+++ stories/screen/product_details.stories.tsx
@@ -3,6 +3,8 @@ import * as React from 'react';
 import * as mobile_1 from '../../src/screen/product_details/product_details_mobile_1.jpg';
 import * as desktop_1 from '../../src/screen/product_details/product_details_desktop_1.png';
 import * as desktop_2 from '../../src/screen/product_details/product_details_desktop_2.png';
+import * as desktop_2_description from '../../src/screen/product_details/product_details_desktop_2_with_description.jpg';
+import * as desktop_2_review from '../../src/screen/product_details/product_details_desktop_2_with_review.jpg';
 import ProductDetailsScreen from '../../src/screen';
 
 
@@ -19,6 +21,8 @@ const Template: Story = args => <ProductDetailsScreen {...args} />;
 export const Mobile_1 = Template.bind({});
 export const Desktop_1 = Template.bind({});
 export const Desktop_2 = Template.bind({});
+export const Desktop_2_REVIEW = Template.bind({});
+export const Desktop_2_DESCRIPTION = Template.bind({});
 
 Mobile_1.args = {
     image: mobile_1
@@ -28,4 +32,10 @@ Desktop_1.args = {
 }
 Desktop_2.args = {
     image: desktop_2
+}
+Desktop_2_DESCRIPTION.args = {
+    image: desktop_2_description
+}
+Desktop_2_REVIEW.args = {
+    image: desktop_2_review
 }
