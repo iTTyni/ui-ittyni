af6dd0ff5a9b58e1b1a853a9951b5c997746eeda
khmamed <m.khmalech@gmail.com>
2023-03-10 02:49:58 +0100

    card update

--- src/ui/src/ui_cards.ts
+++ src/ui/src/ui_cards.ts
@@ -1,7 +1,16 @@
-import {styled} from '../../theme';
+import {styled, IThemeInterface} from '../../theme';
 
-export const CardContainer = styled('div')<{
-
+export const StyledCardContainer = styled('div')<{
+  theme: IThemeInterface
+  width? : string
 }>`
   display: flex;
+  width : ${({width})=>width? width: 'inherit'};
+`
+export const StyledCardSection = styled('div')<{
+  theme: IThemeInterface
+  width?: string
+}>`
+  background-color: ${({theme})=>theme.color.thirdColor};
+  width: ${({width})=>width? width : '100%'}
 `
---
+++ stories/ui/card.stories.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import {Meta, Story} from '@storybook/react';
+import {StyledCardContainer, StyledCardSection} from '../../src/ui'
+
+const cardMeta : Meta = {
+  title : 'ui/card',
+  component: StyledCardContainer,
+  parameters: {
+    width: {
+      values: [{
+        name: "width",
+        value: "500px"
+      }]
+    }
+  }
+}
+
+
+export default cardMeta;
+
+const cardTemplate : Story = args => 
+  <StyledCardContainer {...args}>
+    this card container
+  </StyledCardContainer>
+const cardSectionTemplate : Story= args =>
+  <StyledCardSection {...args}>section</StyledCardSection>
+const cardExampleTemplate : Story = ({containerArgs, sectionArgs}) => 
+  <StyledCardContainer {...containerArgs}>
+    <StyledCardSection {...sectionArgs}>
+      section 1
+    </StyledCardSection>
+  </StyledCardContainer>
+export const CardContainer = cardTemplate.bind({})
+export const CardSection = cardSectionTemplate.bind({})
+export const CardExample = cardExampleTemplate.bind({})
+
+CardContainer.args = {
+  width: '400px'
+}
+
+CardExample.args ={
+  containerArgs: {
+    width : '400px'
+  },
+  sectionArgs: {
+    width: '100px'
+  }
+}
+
