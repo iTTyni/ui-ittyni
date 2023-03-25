f916b0b90ced5fed5174cbf2e7395a26656dde71
mkhmalech <m.khmalech@gmail.com>
2023-03-25 22:22:48 +0000

    export modules

--- index.ts
+++ index.ts
@@ -9,3 +9,20 @@ export * from './src/ui/ui_wrappers'
 export * from './src/ui/ui_cards'
 export * from './src/ui/ui_table';
 export * from './src/ui/ui_dropdowns'
+
+// components
+// cards
+export * from './src/components/card/card'
+export * from './src/components/card/cardlist'
+// dropdowns
+export * from './src/components/dropdowns/dropdownMenu'
+
+// modal
+export * from './src/components/modal/modal'
+
+// slider 
+export * from './src/components/slider/Slider'
+export * from './src/components/slider/arrows'
+export * from './src/components/slider/images'
+export * from './src/components/slider/indicators'
+export * from './src/components/slider/thumbnails'
