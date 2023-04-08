bd39d27ff6fdaedf027d183d92072d9184cda9cd
mkhmalech <m.khmalech@gmail.com>
2023-04-08 23:26:07 +0000

    add admin side bar

--- src/layout/header/subheader.tsx
+++ src/layout/header/subheader.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { WrapperSubheader } from '../../ui/ui_wrappers';
+import { Search } from '../search/search';
 
 export const Subheader: React.FC<any> = ({ }) => {
 
 
     return (
         <WrapperSubheader>
-            <h3>Subheader</h3>
+            <Search />
         </WrapperSubheader>
     )
 }
--- src/layout/sides/adminSidebar.tsx
+++ src/layout/sides/adminSidebar.tsx
@@ -1,11 +1,37 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { WrapperLeftSide } from "../../ui/ui_wrappers"
 
-export const AdminSidebar: React.FC<any> = ({ }) => {
-
+export const AdminSidebar: React.FC<any> = ({ user }) => {
 
     return (
         <WrapperLeftSide>
-            side bar
+
+            {user &&
+                <div>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-start',
+                        paddingBottom: '10px',
+                        marginBottom: '10px',
+                        borderBottom: '1px solid #f1f1f1',
+                        alignItems: 'center'
+                    }}>
+                        <div>
+                            <img src={user.picture} height="60" width="60" />
+                        </div>
+                        <div style={{ padding: '5px' }}>
+                            {user.firstName} {user.lastName}
+                        </div>
+                    </div>
+                    <ul style={{ listStyle: 'none' }}>
+                        <li><Link to={`/admin/${user.email.split('@')[0]}/profile`}> Profile </Link></li>
+                        <li><Link to={`/admin/${user.email.split('@')[0]}/settings`}> Settings </Link></li>
+                    </ul>
+                </div>
+            }
+            {!user && <div>connecter vous!!</div>}
+
         </WrapperLeftSide>
     )
 }
