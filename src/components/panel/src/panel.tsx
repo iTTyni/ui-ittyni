00cde33f8a787a7795a4b6968bb8849d3e692c51
mkhmalech <m.khmalech@gmail.com>
2023-06-25 22:09:58 +0100

    update panel

--- src/components/panel/src/panel.tsx
+++ src/components/panel/src/panel.tsx
@@ -1,40 +1,28 @@
 import * as React from 'react';
-import { PanelContainer } from '../../../..';
+import { PanelContainerStyled } from '../../../..';
 
 interface IPanelProps {
     Header?: any
     subHeader?: any
     children: JSX.Element
-    mainTitle?: string
-    rightMenu?: any
-    leftMenu?: any[]
     footer?: JSX.Element
+    customeStyles?: {
+      withborder?: boolean
+    }
 }
 
 export const Panel: React.FC<IPanelProps> = ({
-    children, Header, subHeader, rightMenu, leftMenu, footer,
-    mainTitle
+    children, Header, subHeader, footer
 }) => {
   return(
-    <PanelContainer>
+    <PanelContainerStyled>
         {Header&&<div className='header'>{Header}</div>}
         {subHeader&&<div className='sub_header'>{subHeader}</div>}
         <div className='content'>
-            {
-              mainTitle&&
-              <div className='content_header'>
-                <h2 className='title'>{mainTitle}</h2>
-                {rightMenu&&<div className='leftMenu'>{rightMenu}</div>}
-              </div>
-            }
-              <div className='content_body' style={{display: rightMenu||leftMenu ? 'flex': 'block'}} >
-               
-                <div className='main'>{children}</div>
-                {leftMenu&&<div className='rightMenu'>right menu</div>}
-            </div>
+            {children}
         </div>
         {footer&&<div className='footer'>{footer}</div>}
-    </PanelContainer>
+    </PanelContainerStyled>
   ) ;
 };
 
