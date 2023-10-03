b32f49d23bf406070a4d9881f886bea654f6fdc0
Khmalech Med <m.khmalech@gmail.com>
2023-10-03 13:17:12 +0100

    add card

--- src/components/card/src/CardWithTitleTextAndLink.tsx
+++ src/components/card/src/CardWithTitleTextAndLink.tsx
@@ -14,3 +14,68 @@ export const CardWithTitleTextAndLink: React.FC<any>= ({})=> {
         </StyledCardContainer>
     );
 };
+
+export const CardHeader: React.FC<{
+    title: string,
+    EditMenu?: JSX.Element,
+    NavMenu?: JSX.Element
+}> = ({ title, EditMenu, NavMenu }) => {
+
+    return (
+        <div style={{    display: 'flex', alignItems: 'center'}}>
+            <h2 style={{ padding: '5px' }}>
+                <span style={{ padding: '0 5px' }}>{title} </span>
+            </h2>
+            <span style={{ padding: '0 5px' }}>
+                {EditMenu}
+            </span>
+            <span>{NavMenu}</span>
+        </div>
+       
+    )
+}
+export const CardHeaderWithEditMenu: React.FC<any> = ({ title, isEditMode, edit, cancel, validate, NavMenu }) => {
+
+    return (
+        <CardHeader title={title}
+            EditMenu={<>
+                {isEditMode && <i style={{ padding: '0 5px', color: 'blue', cursor: 'pointer' }}
+                    className="fas fa-edit" onClick={edit}
+                />}
+                {!isEditMode && <>
+                    <i style={{ padding: '0 5px', color: 'red', cursor: 'pointer' }}
+                        className="fas fa-times"
+                        onClick={ cancel } />
+                    <i style={{ padding: '0 5px', color: 'green', cursor: 'pointer' }}
+                        className="fas fa-check"
+                        onClick={validate}
+                    />
+                </>}
+            </>}
+            NavMenu={NavMenu}
+        />
+    )
+}
+export const CardHeaderWithEditMenuAndNav: React.FC<{
+    title: string
+    isEditMode?: boolean
+    setEditMode: () => void
+    cancel: () => void
+    validate: () => void
+    Nav : [{
+        title : string,
+        onClick : React.EventHandler<any>
+    }]
+}> = ({ title, isEditMode, setEditMode, cancel, validate, Nav }) => {
+
+    return (
+        <CardHeaderWithEditMenu
+            title={title}
+            isEditMode={isEditMode}
+            setEditMode={setEditMode}
+            cancel={cancel}
+            validate={validate}
+            NavMenu={Nav}
+        />
+    )
+}
