e27b7af58691ca85bf35718ffa2e01010718e8e0
khmammed <khmammed@gmail.com>
2024-05-23 18:03:43 +0100

    create new dropdownMenu

--- src/components/dropdowns/dropdownMenu.tsx
+++ src/components/dropdowns/dropdownMenu.tsx
@@ -1,13 +1,41 @@
 import * as React from 'react'
-import { DropdownMenuStyle } from '../../../'
+import { DropdownContainerStyled, IconStyled, DropdownMenuStyle, DropdownMenuStyled, MenuItemStyled, MenuFooterStyled, DisconnectButtonStyled } from '../../../'
 
-export const DropdownMenu: React.FC<{title?: string, footer?: any, setOpen?: any}>= ({title, footer})=>{
+export const DropdownMenu: React.FC<{ title?: string, footer?: any, setOpen?: any }> = ({ title, footer }) => {
 
 
-    return(
-        <DropdownMenuStyle>
-            <label>{title}</label>
-            <li>Menu 1</li>
-        </DropdownMenuStyle>
-    )
+  return (
+    <DropdownMenuStyle>
+      <label>{title}</label>
+      <li>Menu 1</li>
+    </DropdownMenuStyle>
+  )
+}
+
+export const DropdownMenuContainer = () => {
+  const [isOpen, setIsOpen] = React.useState<boolean>(false)
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  }
+
+  const handleDisconnect = () => {
+    console.log('user logged out')
+  }
+
+  return (
+    <DropdownContainerStyled>
+      <IconStyled onClick={toggleMenu}>☰</IconStyled>
+      <DropdownMenuStyled isOpen={isOpen}>
+        <MenuItemStyled href='/admin/khmammed'>Account</MenuItemStyled>
+        <MenuItemStyled href='/admin/khmammed'>Account</MenuItemStyled>
+        <MenuItemStyled href='/admin/khmammed'>Account</MenuItemStyled>
+        <MenuItemStyled href='/admin/khmammed'>Account</MenuItemStyled>
+        <MenuFooterStyled>
+          <DisconnectButtonStyled onClick={handleDisconnect}>
+            Disconnect
+          </DisconnectButtonStyled>
+        </MenuFooterStyled>
+      </DropdownMenuStyled>
+    </DropdownContainerStyled>
+  )
 }
