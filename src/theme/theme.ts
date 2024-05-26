7593b24ffbee32ded56c4034fd26f837f260e200
khmammed <khmammed@gmail.com>
2024-05-26 16:13:54 +0100

    add five color to theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -12,7 +12,8 @@ export const darkTheme: IThemeInterface = {
     primaryColor: "#e9e9eb",
     secondaryColor: "#2a2c39",
     thirdColor: "#F2F4F6",
-    fourthColor: "#423030"
+    fourthColor: "#423030",
+    fiveColor: "#fbfafd"
   },
   font: {
     fontSize: 12,
@@ -33,7 +34,8 @@ export const lightTheme: IThemeInterface = {
     primaryColor: "#e9e9eb",
     secondaryColor: "#2a2c39",
     thirdColor: "#F2F4F6",
-    fourthColor: "#423030"
+    fourthColor: "#423030",
+    fiveColor: "#fbfafd"
   },
   font: {
 
