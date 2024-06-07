e9ed28e7a4ce58b97339295f416de139a2d90bb8
khmammed <khmammed@gmail.com>
2024-06-07 08:39:07 +0100

    fix sorting bug

--- src/components/table/dataTable.tsx
+++ src/components/table/dataTable.tsx
@@ -119,9 +119,11 @@ const Datalist: React.FC<DatalistProps> = ({ data }) => {
 
     const sortedData = [...filteredData].sort((a, b) => {
       if (a[key] < b[key]) {
+      if (a[key]?.toLowerCase() < b[key]?.toLowerCase()) {
         return direction === 'ascending' ? -1 : 1;
       }
       if (a[key] > b[key]) {
+      if (a[key]?.toLowerCase() > b[key]?.toLowerCase()) {
         return direction === 'ascending' ? 1 : -1;
       }
       return 0;
